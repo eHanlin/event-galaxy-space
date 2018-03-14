cd87a8809f303bcdc9af49adee3f70c56f0420cc
AsoTiesto <cute17852337569@gmail.com>
2018-03-14 15:52:10 +0800

    no message

--- src/js/galaxy-space/event-award.js
+++ src/js/galaxy-space/event-award.js
@@ -1,8 +1,19 @@
 define(['jquery'], $ => {
   require(['ajax'], ajax => {
     ajax('GET', 'http://127.0.0.1:8080/chest/award')
-    .then((data) => {
-      console.log(data)
-    })
+      .then((data) => {
+        let index = 0
+        let awards = data.content
+        console.log(awards)
+
+        for (let award in awards) {
+          let awardId = award.split('#')[0]
+          let value = awards[award]
+          console.log(awardId)
+          console.log(value)
+
+          $('.award-box .block1:eq('index')')
+        }
+      })
   })
 })
