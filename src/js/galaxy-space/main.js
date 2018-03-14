eeaded7ec2cd0f04758e17c79e07e926ab83c4f9
infinityAlive <sonic@ehanlin.com.tw>
2018-03-14 14:57:05 +0800

    delete jconfirm.js

--- src/js/galaxy-space/main.js
+++ src/js/galaxy-space/main.js
@@ -1,25 +1,30 @@
 require.config({
   shim: {
-    jqueryConfirm: ['jquery']
+    dialog: ['jquery']
   },
 
   paths: {
-    'jquery': ['../lib/jquery-3.3.1.min'],
-    jqueryConfirm: ['../lib/jquery-confirm.min'],
-    'w3': ['../lib/w3'],
-    'ajax': ['../module-utils/ajax'],
+    jquery: ['../lib/jquery-3.3.1.min'],
+    dialog: ['../lib/dialog-plus'],
+    w3: ['../lib/w3'],
+    ajax: ['../module-utils/ajax'],
     popup: ['../module-utils/popup'],
     'event-chest': ['./event-chest'],
     'event-click-link': ['./event-click-link'],
     'event-galaxy-space': ['./event-galaxy-space']
+  },
+
+  map: {
+    '*': {
+      'jQuery': 'jquery'
+    }
   }
 })
 
-require(['jquery', 'w3', 'ajax', 'jqueryConfirm', 'popup',
-  'event-chest', 'event-click-link', 'event-galaxy-space'], ($, w3, ajax, jqueryConfirm, popup) => {
+require(['jquery', 'w3', 'dialog', 'popup',
+  'event-chest', 'event-click-link', 'event-galaxy-space'], ($, w3, dialog, popup) => {
   $('.start-btn').on('click', () => {
-    $.alert(popup.alert('GG', 'yy', () => {
-      console.log('power')
-    }))
+    let d = popup.confirm('不', 'ydddddddddd')
+    d.showModal()
   })
 })
