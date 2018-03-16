ac4dda021bf4f099b0e18f1ee03ee8c65285d4b1
AsoTiesto <cute17852337569@gmail.com>
2018-03-16 12:55:30 +0800

    新增 totalAssets 顯示e幣和寶石

--- src/js/galaxy-space/event-galaxy-space.js
+++ src/js/galaxy-space/event-galaxy-space.js
@@ -1,1 +1,11 @@
-define(['jquery'], $ => {})
+define(['jquery'], $ => {
+  require(['ajax'], ajax => {
+    ajax(
+      'GET',
+      'http://localhost:9090/currencyBank/totalAssets/one'
+    ).then(data => {
+      $('.coins').append(data.content.coins)
+      $('.gems').append(data.content.gems)
+    })
+  })
+})
