a2e52dc5ee4bf44db996fa7d962ea3e62440b5bd
AsoTiesto <cute17852337569@gmail.com>
2018-03-19 12:17:32 +0800

    更新新圖  調整位置

--- src/js/galaxy-space/event-chest.js
+++ src/js/galaxy-space/event-chest.js
@@ -16,14 +16,12 @@ define(['jquery', 'popup'], ($, popup) => {
             let chestStatus = chest.status
             let platformColor = chest.colorPlatform
 
-            if (chestStatus === 'LOCKED') {
-
-            }
-
             $(`.platform-${platformColor}`)
               .append(`<img class="chest${chestLevel}" title="chest${chestLevel}" src="./img/chest/chest${chestLevel}.png">`)
             $(`.platform-${platformColor} .start-btn`).css('display', '')
             $(`.platform-${platformColor} .upgrade-btn`).css('display', '')
+
+            /* 啟動寶箱 */
             $(`.platform-${platformColor} .start-btn`).on('click', () => {
               popup.dialog('確定要啟動寶箱嗎？', '', () => {
                 ajax('GET', `http://localhost:8080/chest/coolDownTime/${chestId}`)
@@ -42,14 +40,44 @@ define(['jquery', 'popup'], ($, popup) => {
               })
             })
 
+            /* 升級寶箱 */
             $(`.platform-${platformColor} .upgrade-btn`).on('click', event => {
-              popup.dialog('確定要升級寶箱嗎？', '', (upLevel) => {
-                let putData = {
-                  level: upLevel
-                }
-                ajax('PUT', `http://localhost:8080/chest/upgrade/${chestId}`, putData)
+              popup.dialog('確定要升級寶箱嗎？', '', () => {
+                ajax('GET', 'http://localhost:9090/currencyBank/totalAssets/one')
                   .then((data) => {
-                    console.log(data)
+                    let totalCoins = data.content.coins
+                    let totalGems = data.content.gems
+                    let upLevel = chestLevel + 1
+                    ajax('GET', `http://localhost:8080/chest/condition/level${upLevel}`)
+                      .then((data) => {
+                        let upChestLevelCoins = data.content.content.coins
+                        let upChestLevelGems = data.content.content.gems
+                        let levelUpData = {
+                          level: upLevel
+                        }
+                        if (totalCoins - upChestLevelCoins >= 0 && totalGems - upChestLevelGems >= 0) {
+                          ajax('PUT', `http://localhost:8080/chest/upgrade/${chestId}`, levelUpData)
+                            .then((data) => {
+                              $(`.platform-${platformColor} .chest${chestLevel}`).remove()
+                              if (data.message.indexOf('success') >= 0) {
+                                $(`.platform-${platformColor}`)
+                                  .append(`<img class="chest${upLevel}" title="chest${upLevel}" src="./img/chest/chest${upLevel}.png">`)
+                              }
+
+                              if (data.message.indexOf('failure') >= 0) {
+                                $(`.platform-${platformColor}`)
+                                  .append(`<img class="chest${upLevel - 1}" title="chest${upLevel - 1}" src="./img/chest/chest${upLevel - 1}.png">`)
+                              }
+
+                              console.log(data)
+                            })
+                        } else if (totalCoins - upChestLevelCoins < 0 || totalGems - upChestLevelGems < 0) {
+                          let nowCoins = totalCoins - upChestLevelCoins
+                          let nowGems = totalGems - upChestLevelGems
+                          console(nowCoins)
+                          console(nowGems)
+                        }
+                      })
                   })
               })
             })
--- src/js/galaxy-space/event-galaxy-space.js
+++ src/js/galaxy-space/event-galaxy-space.js
@@ -1,5 +1,8 @@
 define(['jquery'], $ => {
   require(['ajax'], ajax => {
+    ajax('GET', 'http://127.0.0.1:8080/chest/')
+      .then(data => {})
+
     ajax(
       'GET',
       'http://localhost:9090/currencyBank/totalAssets/one'
