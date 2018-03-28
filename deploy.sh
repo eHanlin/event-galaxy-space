90aed3d5b5b5eb7546c806e0bd124f8bf33c7548
AsoTiesto <cute17852337569@gmail.com>
2018-03-28 17:27:16 +0800

    更改host name

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test="http://test.ehanlin.com.tw/event/api/Deploy"
 production="http://www.ehanlin.com.tw/event/api/Deploy"
-repository="{\"Repository\":\"event-galaxy-space\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"galaxy-space\"}"
+repository="{\"Repository\":\"event-galaxy-space\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\",\"Name\":\"galaxySpace\"}"
 contentTypeJson="Content-Type: application/json"
 
 case "${TRAVIS_TAG}" in
