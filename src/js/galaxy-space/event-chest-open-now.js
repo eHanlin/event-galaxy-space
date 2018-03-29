129f3610fc3348ab535bf33d35d0c1b97c610307
infinityAlive <sonic@ehanlin.com.tw>
2018-03-29 11:49:54 +0800

    修復內容

--- src/js/galaxy-space/event-chest-open-now.js
+++ src/js/galaxy-space/event-chest-open-now.js
@@ -1,45 +1,45 @@
 define(['jquery', 'ajax', 'confirmPopup', 'eventStatusDo'], ($, ajax, confirmPopup, eventStatusDo) => {
   return (chest, targets) => {
     let seconds
-    ajax('GET', `https://test.ehanlin.com.tw/chest/coolDownTime/${chest.id}`)
+    ajax('GET', `/chest/coolDownTime/${chest.id}`)
       .then(data => {
         seconds = data.content
 
         console.log(seconds)
-        return ajax('GET', `https://test.ehanlin.com.tw/chest/condition/openImmediately`)
+        return ajax('GET', `/chest/condition/openImmediately`)
       }).then(data => {
-        let openImmediatelyData = data.content
-        let consume = openImmediatelyData['content']
-        let everySecondsHour = 3600
-        let remainHours = Math.ceil(seconds / everySecondsHour)
-        let deductGems = remainHours * consume.everyHourDeductGems
+      let openImmediatelyData = data.content
+      let consume = openImmediatelyData['content']
+      let everySecondsHour = 3600
+      let remainHours = Math.ceil(seconds / everySecondsHour)
+      let deductGems = remainHours * consume.everyHourDeductGems
 
-        confirmPopup.dialog(`<h2>立即開啟寶箱需花費${deductGems}個寶石</h2><h3>確定要立即開啟寶箱嗎？<h3>`, () => {
-          ajax('PUT', `https://test.ehanlin.com.tw/chest/open/immediately/${chest.id}`, {
-            deductGems: deductGems
-          }).then(data => {
-            let originalGems = $('#gems').text()
-            let finalGems = data.content.finalGems
-            let insufficientGems = originalGems - deductGems
-            let statusData = {
-              status: 'OPEN'
-            }
+      confirmPopup.dialog(`<h2>立即開啟寶箱需花費${deductGems}個寶石</h2><h3>確定要立即開啟寶箱嗎？<h3>`, () => {
+        ajax('PUT', `/chest/open/immediately/${chest.id}`, {
+          deductGems: deductGems
+        }).then(data => {
+          let originalGems = $('#gems').text()
+          let finalGems = data.content.finalGems
+          let insufficientGems = originalGems - deductGems
+          let statusData = {
+            status: 'OPEN'
+          }
 
-            /* 檢查餘額是否足夠 */
-            if (insufficientGems < 0) {
-              confirmPopup.confirm(`你的寶石不足${insufficientGems * -1}個`, `欠一屁股債了~ 快做題吧 !`, () => {})
-              return false
-            }
+          /* 檢查餘額是否足夠 */
+          if (insufficientGems < 0) {
+            confirmPopup.confirm(`你的寶石不足${insufficientGems * -1}個`, `欠一屁股債了~ 快做題吧 !`, () => {})
+            return false
+          }
 
-            ajax('PUT', `https://test.ehanlin.com.tw/chest/status/${chest.id}`, statusData)
-              .then(eventStatusDo.open.bind(eventStatusDo.open, chest, targets))
+          ajax('PUT', `/chest/status/${chest.id}`, statusData)
+            .then(eventStatusDo.open.bind(eventStatusDo.open, chest, targets))
 
-            require(['eventCountUp'], eventCountUp => {
-              eventCountUp('gems', originalGems, finalGems)
-              window.location.reload()
-            })
+          require(['eventCountUp'], eventCountUp => {
+            eventCountUp('gems', originalGems, finalGems)
+            window.location.reload()
           })
-        }, () => { /* 取消 */ })
-      })
+        })
+      }, () => { /* 取消 */ })
+    })
   }
 })
