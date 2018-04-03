347cbce85ea501a7ad25e721f72ff6faf238aa5f
infinityAlive <sonic@ehanlin.com.tw>
2018-04-03 17:33:25 +0800

    修改雲端銀行欄位呈現內容

--- src/js/currency-bank/main.js
+++ src/js/currency-bank/main.js
@@ -3,34 +3,9 @@ require(['config'], () => {
     require(['ajax', 'moment', 'momentLocales'], (ajax, moment, momentLocales) => {
       ajax('GET', `/currencyBank/transaction`)
         .then((jsonData) => {
-          return jsonData.content
-        })
-        .then((transactions) => {
-          let retrieve = () => {
-            let index
-            for (index in transactions) {
-              let transaction = transactions[index]
-              let field
-              for (field in transaction) {
-                if (field === 'updateTime') {
-
-                } else if (field === 'currencyQuantity') {
-                  transaction['coins'] = transaction[field].coins ? transaction[field].coins : 0
-                  transaction['gems'] = transaction[field].gems ? transaction[field].gems : 0
-                } else if (field === 'action') {
-                  if (transaction[field].indexOf('ADD') >= 0) {
-                    transaction[field] = '增加'
-                  } else {
-                    transaction[field] = '消耗'
-                  }
-                }
-              }
-            }
-            return transactions
-          }
-
+          let transactions = jsonData.content
           $('#table').bootstrapTable({
-            data: retrieve(),
+            data: transactions,
             striped: true,
             pageSize: 15,
             pageList: [15],
