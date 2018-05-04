b85219b69c090997c2a13a9a4e266883a2e7d520
AsoTiesto <cute17852337569@gmail.com>
2018-05-04 17:07:56 +0800

    https://github.com/eHanlin/tutor/issues/3676 修復 totalAssets無法顯示資產總額 api 的 bug

--- src/js/galaxy-space/event-total-assets.js
+++ src/js/galaxy-space/event-total-assets.js
@@ -1,5 +1,5 @@
 define(['jquery', 'ajax', 'eventCountUp'], ($, ajax, eventCountUp) => {
-  ajax('GET', `/currencyBank/totalAssets`)
+  ajax('GET', `/currencyBank/totalAssets/one`)
     .then(data => {
       let finalCoins = data.content.coins
       let finalGems = data.content.gems
