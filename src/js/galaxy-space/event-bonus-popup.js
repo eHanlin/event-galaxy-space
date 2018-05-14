a5aba1efd1acbf0927788b02f5cc841e08392c8f
AsoTiesto <cute17852337569@gmail.com>
2018-05-14 16:30:05 +0800

    新增功能：增加 flag 判斷使用者 cookie 是否該顯示活動圖 （正式機）

--- src/js/galaxy-space/event-bonus-popup.js
+++ src/js/galaxy-space/event-bonus-popup.js
@@ -1,11 +1,16 @@
-define(['jquery', 'cookie', 'ajax'], ($, Cookie, ajax) => {// eslint-disable-line
+define(['jquery', 'cookie', 'ajax'], ($, Cookie, ajax) => { // eslint-disable-line
   let isBonusPopup = Cookie.get('isBonusPopup')
   let bonusPopupTarget = $('#bonus-popup')
 
   if (!isBonusPopup) {
     ajax('GET', `/chest/condition/bonusPopup`)
       .then(jsonData => {
-        let image = jsonData.content.content.image
+        let bonusPopupContent = jsonData.content.content
+        let image
+        if (!bonusPopupContent.show) {
+          return
+        }
+        image = jsonData.content.content.image
         bonusPopupTarget.css('background-image', `url(${image})`)
         bonusPopupTarget.addClass('bonus-popup-show')
         setTimeout(() => {
