d17638e05af6bf855d8c0338e80d78c70dbd6fc3
AsoTiesto <cute17852337569@gmail.com>
2018-05-14 16:43:19 +0800

    調整功能：調整totalAssets api 路徑（正式機）

--- src/js/galaxy-space/event-total-assets.js
+++ src/js/galaxy-space/event-total-assets.js
@@ -1,5 +1,5 @@
 define(['jquery', 'ajax', 'eventCountUp'], ($, ajax, eventCountUp) => {// eslint-disable-line
-  ajax('GET', `/currencyBank/totalAssets`)
+  ajax('GET', `/currencyBank/totalAssets/one`)
     .then(data => {
       let finalCoins = data.content.coins
       let finalGems = data.content.gems
