7505ba21d528b87d3b49134111897f7830827388
infinityAlive <sonic@ehanlin.com.tw>
2018-07-02 20:45:20 +0800

    調整停止掉落寶箱

--- src/js/galaxy-space/event-bonus-popup.js
+++ src/js/galaxy-space/event-bonus-popup.js
@@ -2,24 +2,40 @@ define(['jquery', 'cookie', 'ajax'], ($, Cookie, ajax) => { // eslint-disable-li
   let isBonusPopup = Cookie.get('isBonusPopup')
   let bonusPopupTarget = $('#bonus-popup')
 
+  let popupHandler = (image) => {
+    bonusPopupTarget.css('background-image', `url(${image})`)
+    bonusPopupTarget.addClass('bonus-popup-show')
+
+    $('#bonus-popup').on('click', event => {
+      $(event.currentTarget).remove()
+    })
+
+    Cookie.set('isBonusPopup', true, {
+      expire: 1
+    })
+  }
+
+  let generalPopup = () => {
+    ajax('GET', `/chest/condition/popup`)
+      .then(jsonData => {
+        let popup = jsonData.content
+        let image = popup.content.image
+        if (!image) {
+          return
+        }
+        popupHandler(image)
+      })
+  }
+
   if (!isBonusPopup) {
     ajax('GET', `/currencyMission/admin/eventRule`)
       .then(jsonData => {
         let image = jsonData.content
         if (!image) {
-          return
+          generalPopup()
+        } else {
+          popupHandler(image)
         }
-
-        bonusPopupTarget.css('background-image', `url(${image})`)
-        bonusPopupTarget.addClass('bonus-popup-show')
-
-        $('#bonus-popup').on('click', event => {
-          $(event.currentTarget).remove()
-        })
-
-        Cookie.set('isBonusPopup', true, {
-          expire: 1
-        })
       })
   }
 })
